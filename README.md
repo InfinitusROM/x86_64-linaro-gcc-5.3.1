4fefa0601c4e80fb2cf186dc31a0287ff3244491
sayeed99 <sayeed.afridi2009@gmail.com>
2016-03-30 14:02:57 +0530

    Linaro-GCC 5.3.1

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+                       SETUP
+
+RUN ./setup to setup the toolcahin for use
+
+had to zip a few files cause github was throwing errors due to their size.
+
+                  Linaro-GCC 5.3.1
+
+toolchain for x86_64 architecture for c/c++ compilation , compiled from linaro source with graphite and link time optimizations(lto).
+
---
+++ lib/gcc/x86_64-unknown-linux-gnu/5.3.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-5.3.1/configure --target= --prefix=/home/cyborg/zenfone/buildtool/sayeed/x86_64-gcc5.3.1/ --with-newlib --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --disable-multilib --enable-shared --disable-libssp --with-gcc-version=Flare-Linaro";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
